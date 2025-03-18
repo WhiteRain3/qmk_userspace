cf1456104b6d72e5cde91bb0237bb334822a22e6
WhiteRain3 <sadykovas.danielius@gmail.com>
2025-03-18 23:29:11 +0200

    fixed

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/custom/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/custom/keymap.c
@@ -197,6 +197,8 @@ void rgb888_to_hsv(uint8_t r, uint8_t g, uint8_t b, uint8_t *h, uint8_t *s, uint
     }
 }
 
+uint8_t h_fg, s_fg, v_fg;
+uint8_t h_bg, s_bg, v_bg;
 uint32_t animate_text(uint32_t trigger, void *ctx) {
 
     if (!display || !my_font || !anim_text) return 0;
@@ -210,8 +212,7 @@ uint32_t animate_text(uint32_t trigger, void *ctx) {
     int16_t width = qp_textwidth(my_font, partial);
     int16_t x = (240 - width) / 2;
     int16_t y = (240 - my_font->line_height) / 2;
-    uint8_t h_fg, s_fg, v_fg;
-    uint8_t h_bg, s_bg, v_bg;
+
     if (anim_step == 0)
         {rgb565_to_rgb888(current_text, &r, &g, &b);
         rgb888_to_hsv(r, g, b, &h_fg, &s_fg, &v_fg);
@@ -220,12 +221,12 @@ uint32_t animate_text(uint32_t trigger, void *ctx) {
         rgb888_to_hsv(r, g, b, &h_bg, &s_bg, &v_bg);
 
         qp_circle(display, 120, 120, 121, h_bg, s_bg, v_bg, true);   // outer ring (border)
-        qp_circle(display, 120, 120, 115, 0, 0, 0, true); // inner cutout
-
-        qp_drawtext_recolor(display, x, y, my_font, partial,
-            h_fg, s_fg, v_fg,
-            h_bg, s_bg, v_bg
-    );}
+        qp_circle(display, 120, 120, 115, 0, 0, 0, false); // inner cutout
+        }
+    qp_drawtext_recolor(display, x, y, my_font, partial,
+        h_fg, s_fg, v_fg,
+        h_bg, s_bg, v_bg
+    );
     qp_flush(display);
 
     if (anim_text[anim_step] != '\0') {
